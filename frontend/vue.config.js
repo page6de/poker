dafd2c3bf68d5888fd4a04f5c48528544283007c
Tim Vogt <tv@page6.de>
2023-08-21 15:16:22 +0200

    Chnaged Frontend App Title

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  outputDir: '../backend/public'
+  outputDir: '../backend/public',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Scrum Poker',
+    },
+  }
 })
